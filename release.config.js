f2f855c86a7c16eb5d96fb5a8ee297b76f7add15
bramiller1989 <bramiller1989@gmail.com>
2021-01-05 19:15:24 -0600

    fix: fix description add semantic release

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/bramiller1989/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
